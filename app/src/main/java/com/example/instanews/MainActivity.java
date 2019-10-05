90e2508b249223c7bb20dc8e37f567330c868055
Roelifela <rodrigo_cordeiro@hotmail.com>
2019-10-05 01:29:45 -0300

    linkando botões

--- app/src/main/java/com/example/instanews/MainActivity.java
+++ app/src/main/java/com/example/instanews/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.instanews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button buttonSaude;
+    private Button buttonMusica;
+    private Button buttonComida;
+    private Button buttonCasa;
+    private Button buttonCiencia;
+    private Button buttonModa;
+    private Button buttonFilme;
+    private Button buttonBeleza;
+    private Button buttonCultura;
+    private Button buttonViagem;
+    private Button buttonEsporte;
+    private Button buttonMundo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSaude = findViewById(R.id.);
+
+
     }
 }
